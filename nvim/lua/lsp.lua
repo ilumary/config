b797889f56e59bf64e44a0e10691078f92436d03
ilumary <christoph.britsch@icloud.com>
2024-05-20 10:27:31 +0200

    diabled annoying error underlining

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -83,3 +83,10 @@ cmp.setup({
     { name = "buffer" },
   },
 })
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        -- Disable underline, it's very annoying
+        underline = false,
+    })
+
